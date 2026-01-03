e3dfb0eb7972cd23140529e20e91b858193164a0
Arash Hatami <hatamiarash7@gmail.com>
2026-01-03 09:25:57 +0330

    chore: update examples

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_version = ">= 1.5"
+
+  required_providers {
+    arvancloud = {
+      source  = "terraform.arvancloud.ir/arvancloud/arvancloud"
+      version = ">= 0.2.2"
+    }
+  }
+}
+
+provider "arvancloud" {
+  api_key = var.arvancloud_api_key
+}
+
 module "cdn_load_balancer" {
   source = "../../"
 
--- examples/basic/variables.tf
+++ examples/basic/variables.tf
@@ -1,3 +1,9 @@
+variable "arvancloud_api_key" {
+  description = "ArvanCloud API key for authentication"
+  type        = string
+  sensitive   = true
+}
+
 variable "domain" {
   description = "The domain name to configure the load balancer for"
   type        = string
